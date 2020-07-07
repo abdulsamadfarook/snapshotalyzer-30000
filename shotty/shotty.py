a9c88eb3a041c370f2d8d2766674cfcb41ede313
Samad Don <samadbhai@Muhammads-MacBook-Pro.local>
2020-07-07 12:15:31 -0400

    Added snapshot functionality

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -16,8 +16,68 @@ def filter_instances(project):
     return instances
 
 @click.group()
+def cli():
+    """Shotty manages instances and snapshots"""
+
+@cli.group('snapshots')
+def snapshots():
+    """ Command for volumes """
+@snapshots.command('list')
+@click.option('--project', default=None, help='Only snapshots with a project (tag Project:<Name>)')
+def list_snapshots(project):
+    "List snapshots"
+    instances = filter_instances(project)
+
+    for i in instances:
+        for v in i.volumes.all():
+            for s in v.snapshots.all():
+                print(','.join((
+                s.id,
+                v.id,
+                i.id,
+                s.state,
+                s.progress,
+                s.start_time.strftime("%c")
+            )))
+    return
+
+@cli.group('volumes')
+def volumes():
+    """ Command for volumes """
+@volumes.command('list')
+@click.option('--project', default=None, help='Only volumes with a project (tag Project:<Name>)')
+def list_volumes(project):
+    "List EC2 volumes"
+    instances = filter_instances(project)
+
+    for i in instances:
+        for v in i.volumes.all():
+            print(','.join((
+                v.id,
+                i.id,
+                v.state,
+                str(v.size) + "Gib",
+                v.encrypted and "Encrypted" or "Not Encrypted"
+            )))
+    return
+
+
+@cli.group('instances')
 def instances():
     """ Command for instances """
+
+@instances.command('snapshot', help="Create a Snapshot of all volumes")
+@click.option('--project', default=None, help='Only instances with a project (tag Project:<Name>)')
+def create_snapshot(project):
+    "Create Snapshot"
+    instances = filter_instances(project)
+
+    for i in instances:
+        i.stop()
+        for v in i.volumes.all():
+            print("Creating a snapshot of {0}".format(v.id))
+            v.create_snapshot(Description="Created by script")
+    return
 
 @instances.command('list')
 @click.option('--project', default=None, help='Only instances with a project (tag Project:<Name>)')
@@ -65,4 +125,4 @@ def start_instances(project):
 
 
 if __name__ == '__main__':
-    instances()
+    cli()
